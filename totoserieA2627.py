4f838e5f03e6106099beade82e8e23ae261a05f4
detben <detsalemme@gmail.com>
2026-09-16 02:54:33 +0200

    Update Google Sheets link in totoserieA2627.py

--- totoserieA2627.py
+++ totoserieA2627.py
@@ -16,7 +16,7 @@ ONEDRIVE_LINK = "https://1drv.ms/x/c/37257a5e51e01cb8/IQBsF-NEdoduQI8-Jzl1AwRXAQ
 # ==========================================
 # 2. LINK GOOGLE SHEETS (Per i Pronostici)
 # ==========================================
-GOOGLE_LINK = "INCOLLA_QUI_IL_LINK_DI_GOOGLE_SHEETS"
+GOOGLE_LINK = "https://docs.google.com/spreadsheets/d/1Qzd-5U0ixS5dnwEXaTovX8XpVLoentoS1cfZL3Hg8p0/edit?usp=sharing"
 
 def converti_link_onedrive(link: str) -> str:
     """Forza il parametro di download sul link fornito."""
