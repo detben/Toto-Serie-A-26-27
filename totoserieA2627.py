5cf4e1cc7753ea619c130c91b0a093a50ece8d18
detben <detsalemme@gmail.com>
2026-09-19 17:11:43 +0200

    Adjust starting column and range for scores

--- totoserieA2627.py
+++ totoserieA2627.py
@@ -338,10 +338,10 @@ if excel_file is not None:
         # LOGICA DI SCORRIMENTO ORIZZONTALE:
         # Colonna B corrisponde all'indice 1.
         # Larghezza tabella 14 colonne + Spazio 1 colonna = Salto di 15 colonne
-        col_partenza_punteggi = 1 + ((numero_giornata_punteggi - 4) * 15)
+        col_partenza_punteggi = 1 + ((numero_giornata_punteggi - 1) * 15)
         
         # Genera automaticamente la lista delle 14 colonne da leggere
-        colonne_punteggi = [col_partenza_punteggi + i for i in range(14)]
+        colonne_punteggi = [col_partenza_punteggi + i for i in range(11)]
         
         # Legge i dati dal foglio "Punteggi"
         df_punteggi = leggi_sezione_classifica(
